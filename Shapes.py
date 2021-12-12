b00c6ce322de361999492df0a08a14cca62b4320
Luis Miguel Martinez <lmiguel.martinez@edu.uah.es>
2021-12-12 23:58:53 +0100

    Game window set up. Shapes library started.

---
+++ Shapes.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame import draw
+
+
+class Shape:
+
+    def __init__(self, shape_type, dimensions = [], color = ()):
---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame.time
+
+from SimulationWindow import GameWindow
+
+myWindow = GameWindow(width = 1200, height = 800)
+game_clock = pygame.time.Clock()
+
+def main():
+    myWindow.window_config()
+    #Main game loop
+    while not (myWindow.exit_window()):
+        game_clock.tick(60)
+        myWindow.draw_frame()
+
+if __name__ == "__main__":
+    main()
